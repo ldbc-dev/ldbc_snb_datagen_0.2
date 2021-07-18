900554a251f2a6dc82120bd74894ce82e9433d88
Gabor Szarnyas <szarnyasg@gmail.com>
2021-07-18 17:10:09 +0200

    Add scripts to compare local vs. EMR results

---
+++ compare-results.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export SF=0.003
+
+rm -rf sf${SF}-emr/
+LDBC_DATA_DIR_TIMESTAMP=$(aws s3 ls s3://ldbc-snb-datagen-bi-2021-07/results/sf${SF}-test/runs/ | grep -o '[0-9][0-9]*_[0-9][0-9]*' | head -n 1)
+aws s3 cp --recursive s3://ldbc-snb-datagen-bi-2021-07/results/sf${SF}-test/runs/${LDBC_DATA_DIR_TIMESTAMP}/social_network/csv/raw/composite-merged-fk sf${SF}-emr
+aws s3 rm --recursive s3://ldbc-snb-datagen-bi-2021-07/results/sf${SF}-test/runs/${LDBC_DATA_DIR_TIMESTAMP}
+
+export ENTITY=Person
+echo "===== ${ENTITY} ====="
+tail -qn +2 sf${SF}-local/csv/raw/*/*/${ENTITY}/*.csv | wc -l
+tail -qn +2 sf${SF}-emr/*/${ENTITY}/part_*.csv | wc -l
+
+echo "===== ${ENTITY} ====="
+export ENTITY=Forum
+tail -qn +2 sf${SF}-local/csv/raw/*/*/${ENTITY}/*.csv | wc -l
+tail -qn +2 sf${SF}-emr/*/${ENTITY}/part_*.csv | wc -l
+
+echo "---- local ----"
+tail -qn +2 sf${SF}-local/csv/raw/*/*/${ENTITY}/*.csv | sort | head
+echo "----- EMR -----"
+tail -qn +2 sf${SF}-emr/*/${ENTITY}/part_*.csv | sort | head
---
+++ submit-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SF=0.003
+
+./tools/build.sh
+
+# EMR run
+aws s3 cp target/ldbc_snb_datagen_2.11_spark2.4-0.4.0-SNAPSHOT-jar-with-dependencies.jar s3://${BUCKET_NAME}/jars/ldbc_snb_datagen_2.11_spark2.4-0.4.0-SNAPSHOT-jar-with-dependencies.jar
+./tools/emr/submit_datagen_job.py --bucket ${BUCKET_NAME} --az us-east-2a sf${SF}-test ${SF} -- --format csv --mode raw
+
+# local run
+rm -rf sf${SF}-local
+./tools/run.py ./target/ldbc_snb_datagen_${PLATFORM_VERSION}-${DATAGEN_VERSION}.jar -- --format csv --scale-factor ${SF} --mode raw --output-dir sf${SF}-local
